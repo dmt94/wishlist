2576945e95a744cecd6c6be4852e8f1eae51b435
Daevah Taylaran <daevtayl@gmail.com>
2023-03-24 12:09:16 -0700

    add views for create and delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.wish_index, name='index'),
+  path('item/create/', views.ItemCreate.as_view(), name='item_create'),
+  path('item/<int:pk>/delete/', views.ItemDelete.as_view(), name='item_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from .models import Item
+from django.views.generic.edit import CreateView, DeleteView
 
 # Create your views here.
 def wish_index(request):
-  return render(request, 'index.html')
+  item_list = Item.objects.all()
+  return render(request, 'index.html', {
+    'item_list': item_list
+  })
+
+class ItemCreate(CreateView):
+  model = Item
+  fields = ['description']
+  success_url = '/'
+
+class ItemDelete(DeleteView):
+  model = Item
+  success_url = '/'
